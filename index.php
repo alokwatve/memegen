4e13978f04ba20d2a75145fd8f328b8df835f557
Alok <alok.watve@gmail.com>
2016-03-18 23:52:47 -0500

    Basic header setup.

--- index.php
+++ index.php
@@ -3,32 +3,16 @@
 <title>Memegen!</title>
 <script src="jquery.js"></script> 
 
-<script>
-  function handleLoad(e) {
-    console.log('Loaded import: ' + e.target.href);
-  }
-  function handleError(e) {
-    console.log('Error loading import: ' + e.target.href);
-  }
-</script>
+<link rel="import" id="header" href="header.html">
 
-<link rel="import" href="header.html"
-      onload="handleLoad(event)" onerror="handleError(event)">
 </head>
 <body>
-<center><h1> Memegen </h1><br></center>
 
 <script>
-    var link = document.querySelector('link[rel="import"]');
-    var content = link.import;
-
-    // Grab DOM from warning.html's document.
-    var el = content.querySelector('.warning');
-
-    document.body.appendChild(el.cloneNode(true));
-  </script>
-
-
+    var getImport = document.getElementById('header');
+    var content = document.querySelector('link[id="header"]').import;    
+    document.body.appendChild(content.querySelector('.header').cloneNode(true));
+</script>
 <?php
 include 'util.php';
 //echo renderImage('templates/clinton_zero.jpg');
