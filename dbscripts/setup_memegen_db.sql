f980cf667a0789ed7eb55d17ea3d1d4e02e50d18
Alok <alok.watve@gmail.com>
2016-03-20 22:58:23 -0500

    Prepare DB for planned improvements.

--- dbscripts/setup_memegen_db.sql
+++ dbscripts/setup_memegen_db.sql
@@ -1,4 +1,18 @@
 use memegendb;
+
+create table if not exists MemeTemplates(
+    template_id bigint auto_increment primary key,
+    filename varchar(256)
+);
+
+create table if not exists TemplateTags (
+    tag_id bigint auto_increment primary key,
+    template_id bigint,
+    tag varchar(64),
+    index using hash (template_id),
+    foreign key (template_id) references MemeTemplates(template_id) on delete cascade on update cascade
+); 
+
 create table if not exists MemeInfo (
     meme_id bigint auto_increment,
     filename varchar(256) unique,
@@ -6,6 +20,8 @@ create table if not exists MemeInfo (
     downvotes bigint,
     createdby varchar(256),
     createdon datetime,
+    template_id bigint,
+    foreign key (template_id) references MemeTemplates(template_id) on delete cascade on update cascade,
     primary key (meme_id)
 );
 
